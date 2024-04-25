f683a6bdf351bb0ca6311767b18406acc546450b
Emil Lindström <lindstromemil86@gmail.com>
2024-04-25 15:21:39 +0200

    added CSS styles

---
+++ src/Controller/Controller.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Controller extends AbstractController
+{
+    #[Route('/lucky')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+
+    #[Route("/", name: "me")]
+    public function me(): Response
+    {
+        return $this->render('me.html.twig');
+    }
+
+    #[Route("/about", name: "about")]
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+
+    #[Route("/report", name: "report")]
+    public function report(): Response
+    {
+        return $this->render('report.html.twig');
+    }
+}

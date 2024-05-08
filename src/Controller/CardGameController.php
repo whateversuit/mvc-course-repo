419964f91ced2da6ddfc3de54093cdca4e06ef9a
Emil Lindström <lindstromemil86@gmail.com>
2024-05-08 11:47:06 +0200

    created classes with empty logic to make sure first route in CardGameController worked.

--- src/Controller/CardGameController.php
+++ src/Controller/CardGameController.php
@@ -10,3 +10,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+
+class CardGameController extends AbstractController
+{
+    #[Route("/card", name: "card")]
+    public function card(): Response
+    {
+        
+        return $this->render('card.html.twig');
+    }
+
+}

fed019ea5feb6d475137677ea7b66ebb17621813
Emil Lindström <lindstromemil86@gmail.com>
2024-05-08 10:20:31 +0200

    Added sessionController with session route and sesson/delete route. Flash message added to session delete

---
+++ src/Controller/SessionController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SessionController extends AbstractController
+{   
+    #[Route("/session", name: "session")]
+    public function sessionAll(SessionInterface $session): Response
+    {
+    
+        $data = [
+            'session' => $session->all()
+        ];
+
+        return $this->render('session.html.twig', $data);
+    }
+    #[Route("/session/delete", name: "session_delete")]
+    public function sessionDelete(SessionInterface $session): Response
+    {
+        $session->clear();
+        $this->addFlash('success', 'Session deleted successfully.');
+
+        return $this->redirectToRoute('session');
+    }
+}
